393db868ab4846c81382f58806a9fc79b789ac45
Capi Etheriel <barraponto@gmail.com>
2019-05-25 21:29:19 -0300

    cleans a few fields to match our use case

---
+++ data_cleaner.py
@@ -0,0 +1,31 @@
+'''Cleans up some fields from webrobots csv dataset.'''
+import csv
+import json
+from datetime import date
+
+
+def clean_data(row):
+    '''Cleans a few columns from input rows.'''
+    row = row.copy()
+    row['category'] = json.loads(row['category'])['name']
+    row['created_at'] = date.fromtimestamp(int(row['created_at'])).isoformat()
+    row['deadline'] = date.fromtimestamp(int(row['deadline'])).isoformat()
+    row['launched_at'] = date.fromtimestamp(int(row['launched_at'])).isoformat()
+    try:
+        row['location'] = json.loads(row['location'])['displayable_name']
+    except json.JSONDecodeError:
+        pass
+    row['urls'] = json.loads(row['urls'])['web']['project']
+    return row
+
+def main():
+    '''Cleans data from us.csv into us-clean.csv'''
+    with open('./data/us.csv') as csvinput, open('./data/us-clean.csv', 'w') as csvoutput:
+        reader = csv.DictReader(csvinput)
+        writer = csv.DictWriter(csvoutput, reader.fieldnames)
+        writer.writeheader()
+        for row in reader:
+            writer.writerow(clean_data(row))
+
+if __name__ == "__main__":
+    main()
--- kickstart.sh
+++ kickstart.sh
@@ -15,7 +15,9 @@ set -o nounset                                  # Treat unset variables as an er
 mkdir data
 wget https://s3.amazonaws.com/weruns/forfun/Kickstarter/Kickstarter_2019-04-18T03_20_02_220Z.json.gz
 gunzip -c Kickstarter_2019-04-18T03_20_02_220Z.json.gz > ./data/kickstarter.json
-python ./json_to_csv.py
-#cat data/Kickstarter*.csv | pipenv run csvgrep -c country -m US -z 524288 > ./data/usdata.csv
+python ./json_to_csv.py # creates ./data/kickstarter.csv
+pipenv run csvgrep -c country -m US -z 524288 ./data/kickstarter.csv > ./data/us.csv
+python ./data_cleaner.py # creates ./data/us-clean.csv
+#pipenv run csvgrep -c launched_at,deadline -r '^201[4-8]' -z 524288 ./data/us.csv > ./data/us-years.csv
 #cd kickstarter
 #pipenv run scrapy crawl -a file=../data/usdata.csv -o ../data/data.csv ksdata
