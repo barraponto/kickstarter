52d227b527e7558ee6a3fe0675a5ecd878eee107
Capi Etheriel <barraponto@gmail.com>
2019-05-26 13:23:13 -0300

    adds timers to commands in kickstart.sh

--- kickstart.sh
+++ kickstart.sh
@@ -14,15 +14,15 @@ set -o nounset                                  # Treat unset variables as an er
 set -o xtrace
 
 mkdir data
-wget https://s3.amazonaws.com/weruns/forfun/Kickstarter/Kickstarter_2019-04-18T03_20_02_220Z.json.gz
-gunzip -c Kickstarter_2019-04-18T03_20_02_220Z.json.gz > ./data/kickstarter.json
-python ./json_to_csv.py # creates ./data/kickstarter.csv
-pipenv run csvgrep -c country -m US -z 524288 ./data/kickstarter.csv > ./data/us.csv
-python ./data_cleaner.py # creates ./data/us-clean.csv
-pipenv run csvgrep -c launched_at,deadline -r '^201[4-8]' -z 524288 ./data/us-clean.csv > ./data/us-years.csv
-pipenv run csvcut -c id -z 524288 ./data/us-years.csv | sort | uniq -d > ./data/dupes-ids.csv
-pipenv run csvgrep -c id -f ./data/dupes-ids.csv -z 524288 ./data/us-years.csv | pipenv run csvsort -z 524288 -c id > ./data/dupes.csv
-pipenv run csvgrep -c id -f ./data/dupes-ids.csv -i -z 524288 ./data/us-years.csv > ./data/deduped.csv
-python ./dedupe.py # appends deduped.csv with deduped rows
-cd kickstarter
-pipenv run scrapy crawl -a file=../data/deduped.csv -o ../data/scraped.csv ksdata
+time wget https://s3.amazonaws.com/weruns/forfun/Kickstarter/Kickstarter_2019-04-18T03_20_02_220Z.json.gz
+time gunzip -c Kickstarter_2019-04-18T03_20_02_220Z.json.gz > ./data/kickstarter.json
+time python ./json_to_csv.py # creates ./data/kickstarter.csv
+time pipenv run csvgrep -c country -m US -z 524288 ./data/kickstarter.csv > ./data/us.csv
+time python ./data_cleaner.py # creates ./data/us-clean.csv
+time pipenv run csvgrep -c launched_at,deadline -r '^201[4-8]' -z 524288 ./data/us-clean.csv > ./data/us-years.csv
+time pipenv run csvcut -c id -z 524288 ./data/us-years.csv | sort | uniq -d > ./data/dupes-ids.csv
+time pipenv run csvgrep -c id -f ./data/dupes-ids.csv -z 524288 ./data/us-years.csv | pipenv run csvsort -z 524288 -c id > ./data/dupes.csv
+time pipenv run csvgrep -c id -f ./data/dupes-ids.csv -i -z 524288 ./data/us-years.csv > ./data/deduped.csv
+time python ./dedupe.py # appends deduped.csv with deduped rows
+time cd kickstarter
+time pipenv run scrapy crawl -L INFO -a file=../data/deduped.csv -o ../data/scraped.csv ksdata
